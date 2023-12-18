28905b9167f8f1d3292be456eb99367d40dfe8cf
Maxime <69683345+CookieG77@users.noreply.github.com>
2023-12-18 11:34:29 +0100

    Add files via upload

---
+++ FinalWebPage/main.py
@@ -0,0 +1,19 @@
+"""
+main file to launch our project web page.
+"""
+from flask import Flask, render_template, request, redirect, url_for, session, make_response, jsonify
+from cryptography.fernet import Fernet
+
+app = Flask(__name__)
+
+app.secret_key = b"thiswebsiteprotectionisnotgreatbecauseitisnotarealone"
+
+@app.route("/")
+def home():
+    """home page"""
+    return render_template("home.html", connected = True, pfp = None)
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
